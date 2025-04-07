5278c495d3042f787c86d496fa7c32012ba47973
Brandon341 <brandones60@gmail.com>
2025-04-06 19:58:22 -0600

    deploy

--- db.js
+++ db.js
@@ -1,18 +1,20 @@
+// backend/db.js
 const mysql = require('mysql2')
 
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',      // cambia esto
-  password: 'root', // cambia esto
-  database: 'vigilium'  // o el nombre que le hayas puesto
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 })
 
-connection.connect(err => {
+connection.connect((err) => {
   if (err) {
-    console.error('Error de conexión a MySQL:', err)
-    return
+    console.error('❌ Error al conectar con Railway:', err)
+  } else {
+    console.log('✅ Conectado a la base de datos Railway')
   }
-  console.log('✅ Conexión MySQL exitosa')
 })
 
 module.exports = connection
